224b9db997780a319101df91dbd96b15513e917f
Rahul <rajamohanty12345@gmail.com>
2022-02-23 22:06:58 +0530

    shared comps updated

--- src/components/shared/TextInput.js
+++ src/components/shared/TextInput.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Label from "./Label";
 
-const TextInput = ({ type, id, onValueChange, required = false, labelText, value,autocomplete}) => {
+const TextInput = ({ type, onValueChange, labelText, value, autocomplete }) => {
   return (
     <>
-    <Label labelText={labelText} id={id} required={required}/>
+      <Label labelText={labelText} />
       <input
         autoComplete={autocomplete}
         type={type}
-        id={id}
         value={value}
         className="font-serif text-md border-2 mx-auto mb-8 p-2 rounded-md w-72 "
         onChange={(e) => {
